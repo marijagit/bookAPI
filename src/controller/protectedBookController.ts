bd3ef35df25790c581f94bbeab8f948158ee7fcc
Marija Mišović <58690569+marijagit@users.noreply.github.com>
2026-01-19 16:32:19 +0100

    Add GET endpoint for retrieving user reviews

--- src/controller/protectedBookController.ts
+++ src/controller/protectedBookController.ts
@@ -5,6 +5,31 @@ import type { IRequest } from '../interface/IRequest.js';
 
 const protectedBookController = express.Router();
 
+protectedBookController.get('/:isbn', (req: Request, res: Response) => {
+    const isbnTxt = req.params?.isbn as string;
+    if (!isbnTxt) {
+        return res.status(400).json({ message: `Isbn is required!` });
+    }
+
+    const isbn = parseInt(isbnTxt);
+    if (Number.isNaN(isbn)) {
+        return res.status(400).json({ message: `Isbn must be valid!` });
+    }
+
+    const book = books[isbn];
+    if (!book) {
+        return res.status(404).json({ message: `Book with the isbn: ${isbn} could not be found.` });
+    }
+
+    const username = (req as IRequest).user.username;
+    const review = book.reviews[username];
+    if (!book.reviews || !review) {
+        return res.status(404).json({ message: `Book with the isbn: ${isbn} does not have any reviews by the user ${username}.` });
+    }
+
+    return res.status(200).json(review);
+});
+
 protectedBookController.post('/:isbn', (req: Request, res: Response) => {
     const isbnTxt = req.params?.isbn as string;
     const review = req.body?.review;
@@ -59,5 +84,6 @@ protectedBookController.delete('/:isbn', (req: Request, res: Response) => {
 
     return res.status(200).json({ message: `Review from ${username} has been successfully removed.` });
 });
+
 
 export { protectedBookController };
